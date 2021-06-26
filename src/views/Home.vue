152feb2b6f874ed8efd2dbda8203165c8e15d543
Indhara Amerta <indhara.amerta@gmail.com>
2021-06-26 21:04:35 +0700

    login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -269,6 +269,46 @@
         </div>
       </div>
     </div>
+    <!-- LOGIN & REGISTER -->
+    <div class="flex items-center justify-center h-screen bg-gray-100">
+      <div class="relative flex flex-col w-full">
+        <div class="p-5 bg-black shadow-xl mt-60 rounded-3xl m-52">
+          <h2 class="pl-3 text-2xl font-semibold text-white">
+            Welcome To PIPISI
+          </h2>
+          <p class="pl-3 mt-2 text-sm text-white">P C P A R T P I C K E R</p>
+          <div class="pl-3 mt-44">
+            <p class="text-sm text-white">
+              Don't Have Any Account?
+              <a href="#" class="text-sm text-pink-500">Register Here</a>
+            </p>
+          </div>
+        </div>
+        <div
+          class="absolute flex flex-col items-center justify-center w-64 p-2 bg-white shadow-2xl  rounded-xl mt-52 right-72"
+        >
+          <h2 class="m-3 text-2xl font-semibold">PLEASE LOGIN !</h2>
+          <div
+            class="w-12 h-12 mt-2 mb-2 bg-black rounded-full shadow-xl"
+          ></div>
+          <input
+            type="text"
+            placeholder="Email"
+            class="p-2 m-5 bg-transparent border-b-2 border-pink-500  focus:outline-none focus:border-grey-600 w-52"
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            class="p-2 mb-5 bg-transparent border-b-2 border-pink-500  w-52 focus:outline-none focus:border-grey-600"
+          />
+          <button
+            class="w-40 p-2 mt-4 font-semibold text-white bg-black  rounded-xl hover:bg-pink-500 mb-7 focus:outline-none"
+          >
+            LOGIN
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
