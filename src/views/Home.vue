90618bf24e5f21703b55df7b9e52709549a41034
giriputra <giriputra.b@gmail.com>
2021-07-01 18:25:40 +0700

    fixed data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -106,7 +106,7 @@ export default {
                     },
                 },
                 {
-                    header: "Ultra Gamer Intel Gaming PC",
+                    header: "Ultra Gamer AMD Gaming PC",
                     image: "https://cdn.shopify.com/s/files/1/0228/7629/1136/files/gx13-intruder-main-shot.png?v=1569339401",
                     harga: 80000000,
                     cpu: {
